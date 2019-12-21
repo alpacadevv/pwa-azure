3a7152d4e02ae9d9bd7f00603d0d00ae7f9982a7
Lee Jinseop <js428118@gmail.com>
2019-12-21 15:56:43 +0900

    add workbox & register service worker

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => <div>Hello PWA+Azure</div>
+
+const App = () => <div>Hello PWA+Azure!!</div>
 
 ReactDOM.render(<App />, document.getElementById('app'))
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    // make service-worker.js by GenerateSW
+    navigator.serviceWorker.register('/service-worker.js')
+  })
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const WebpackPWAManifest = require('webpack-pwa-manifest')
+const { GenerateSW } = require('workbox-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
@@ -49,6 +50,9 @@ module.exports = {
           sizes: [96, 128, 192, 256, 384, 512],
         }
       ]
+    }),
+    new GenerateSW({
+      include: [/\.html$/, /\.js$/],
     }),
   ],
 }
